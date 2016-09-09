ab87e4b1a2c2ea054afb09ebbcc9bdfbb02a6b3f
whatlife <beew@qq.com>
2016-09-09 08:57:34 +0800

    webpack s

---
+++ demo_0011/src/components/app.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>姓名：{{name}}</h1>
+        <h2>{{age}}</h2>
+    </div>
+</template>
+<style lang="sass">
+    $qwe: #098;
+    body {
+        background-color: $qwe;
+        h1 {
+            background-color: #eee;
+            color:red;
+            transform: translate(10%, 10%); / *测试自动添加前缀 */
+            }
+        h1:hover{
+             height:100px;
+            }
+            h2 {
+            background-color: #999;
+            }
+    }
+</style>
+<script>
+    export default{
+        el: "#app",
+        data() {
+            return {
+                name: "ggg",
+                age: "2q1"
+            }
+        }
+    }
+</script>
---
+++ demo_0011/src/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by liangyl on 2016/9/5 0005.
+ */
+
+
+import Vue from "../node_modules/vue/dist/vue.min";
+
+import app from "./components/app";
+
+Vue.config.debug = true;
+
+new Vue(app);
